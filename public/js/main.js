d4dd84186cbb634d4abddfe2a7a53358f4f60872
DevSomeone <yurisakadev@gmail.com>
2022-05-05 15:21:57 +0800

    Web UI design

--- public/js/main.js
+++ public/js/main.js
@@ -26,6 +26,75 @@ function changePathName(name) {
     document.querySelector("#pathName").innerHTML = name;
 }
 
+function createTaskBox() {
+    // Create a progressing task box
+    var taskBox = document.createElement("div");
+    taskBox.classList.add("taskBox");
+    taskBox.innerHTML = `
+        <div class="tskbx">
+            <div class="taskBoxTitle">
+                <h1>資料處理中...</h1>
+            </div>
+            <div class="taskBoxTasks">
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
+                    </div>
+                    <span class="taskName">sfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskj</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
+                    </div>
+                    <span class="taskName">Test</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-xmark"></i></span>
+                    </div>
+                    <span class="taskName">15215916516516515616515616511561651</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
+                    </div>
+                    <span class="taskName">5461246216421624612626</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
+                    </div>
+                    <span class="taskName">sfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskjsfosng okndskj</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-xmark"></i></span>
+                    </div>
+                    <span class="taskName">Test</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-circle-xmark"></i></span>
+                    </div>
+                    <span class="taskName">15215916516516515616515616511561651</span>
+                </div>
+                <div class="task">
+                    <div class="taskStatus">
+                        <span class="icon"><i class="fa-solid fa-spinner"></i></span>
+                    </div>
+                    <span class="taskName">5461246216421624612626</span>
+                </div>
+            </div>
+        </div>
+    `;
+    document.body.appendChild(taskBox);
+    return taskBox;
+}
+
+function addRunningList(data) {
+    
+};
+
 function loadPageScript(id) {
     if (page.find(e => e.id === id) === undefined) return;
     fetch(`/js/page/${id}.js`).then(res => res.text()).then(res => {
--- public/js/page/home.js
+++ public/js/page/home.js
@@ -2,15 +2,63 @@ function execute() {
     var pageElement = document.querySelector("#mainContent");
 
     if (sessionStorage.getItem("auth") === null) {
+        var reslogin = "";
+        if (localStorage.getItem("resLogin") === null) {
+            reslogin = `
+                <div style="text-align: center;">
+                    您還尚未登入過，請先登入。
+                </div>
+            `;
+        } else {
+            var dt = JSON.parse(localStorage.getItem("resLogin"));
+            dt.forEach(e => {
+                
+            });
+        }
+
         pageElement.innerHTML = `
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h1>未登入</h1>
-                    </div>
+            <div class="login">
+                <h1 class="pageTitle">帳號登入</h1>
+                <div class="loginForm">
+                    <form action="/login" id="lognForm">
+                        <div class="form-group">
+                            <label for="username">學號</label>
+                            <input type="text" class="form-control" id="username" name="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">密碼</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">登入</button>
+                    </form>
                 </div>
             </div>
+            <div class="resLogin">
+                <h1 class="pageTitle">曾經登入的帳號</h1>
+                <div class="loginForm">
+                    ${reslogin}
+            </div>
         `;
+
+        var inputs = document.getElementsByTagName("input");
+        Array.from(inputs).forEach(e => {
+            e.addEventListener("focusin", ev => {
+                e.parentElement.classList.add("active");
+            });
+
+            e.addEventListener("focusout", ev => {
+                if (e.value === "") {
+                    e.parentElement.classList.remove("active");
+                }
+            });
+        });
+
+        var loginForm = document.getElementById("lognForm");
+        loginForm.addEventListener("submit", ev => {
+            ev.preventDefault();
+        });
         return;
+    } else {
+        goPage("/");
     }
 }
